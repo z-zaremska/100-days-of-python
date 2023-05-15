94d970c9dceb929f56311e27b1713f53bc1f27ea
z-zaremska <zuza.zaremska@gmail.com>
2023-05-15 16:24:41 +0200

    Changed components adjustment.

--- Day 29 - Password Manager/main.py
+++ Day 29 - Password Manager/main.py
@@ -6,32 +6,39 @@ from tkinter import *
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 window.title('Password Manager')
 
 canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file='logo.png')
 canvas.create_image(100, 100, image=logo_img)
-canvas.grid(column=0, row=0, columnspan=3)
+canvas.grid(column=1, row=0)
 
+# Labels
 website_label = Label(text='Website:')
 website_label.grid(column=0, row=1)
-website_entry = Entry()
-website_entry.grid(column=1, row=1, columnspan=2)
 
 username_label = Label(text='E-mail\\Username:')
 username_label.grid(column=0, row=2)
-username_entry = Entry()
-username_entry.grid(column=1, row=2, columnspan=2)
 
 password_label = Label(text='Password:')
 password_label.grid(column=0, row=3)
-password_entry = Entry()
+
+# Entries
+website_entry = Entry(width=53)
+website_entry.grid(column=1, row=1, columnspan=2)
+
+username_entry = Entry(width=53)
+username_entry.grid(column=1, row=2, columnspan=2)
+
+password_entry = Entry(width=35)
 password_entry.grid(column=1, row=3)
+
+# Buttons
 generate_password_button = Button(text='Generate Password')
 generate_password_button.grid(column=2, row=3)
 
-add_button = Button(text='Add')
-add_button.grid(column=1, row=4)
+add_button = Button(text='Add', width=45)
+add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
