4b028396ebbde72d644b332c315bec15e2e6cd70
z-zaremska <zuza.zaremska@gmail.com>
2023-05-16 10:51:15 +0200

    Added function searching for already entered passwords to recall them or change data.

--- Day 29 - 30 - Password Manager/main.py
+++ Day 29 - 30 - Password Manager/main.py
@@ -29,14 +29,32 @@ def load_passwords():
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def search_for_website():
+    searched_website = search_entry.get().lower()
+    password_entry.delete(0, END)
+    website_entry.delete(0, END)
+    username_entry.delete(0, END)
+
+    try:
+        searched_username = PASSWORDS[f'{searched_website}']['username']
+        searched_password = PASSWORDS[f'{searched_website}']['password']
+
+        website_entry.insert(0, searched_website)
+        username_entry.insert(0, searched_username)
+        password_entry.insert(0, searched_password)
+
+    except KeyError:
+        messagebox.showinfo(title='Website not found', message=f'Sorry, but website {searched_website.title()} was '
+                                                               f'not found')
+
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
 def add_password():
-    website = website_entry.get()
-    website_entry.delete(8, END)
+    website = website_entry.get().lower()
     username = username_entry.get()
     password = password_entry.get()
-    password_entry.delete(0, END)
 
-    if len(website) < 13 or len(username) < 3 or len(password) < 8:
+    if len(website) < 3 or len(username) < 3 or len(password) < 8:
         messagebox.showinfo(title='No valid data', message='All fields must be fulfilled!')
     else:
         # Pop-up message about successful operation
@@ -51,6 +69,10 @@ def add_password():
             with open('my_passwords.json', 'w') as f:
                 json_passwords = json.dumps(PASSWORDS, indent=3)
                 f.write(json_passwords)
+
+            password_entry.delete(0, END)
+            website_entry.delete(0, END)
+            search_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -67,33 +89,39 @@ canvas.grid(column=1, row=0)
 
 # Labels
 website_label = Label(text='Website:')
-website_label.grid(column=0, row=1)
+website_label.grid(column=0, row=2)
 
 username_label = Label(text='E-mail\\Username:')
-username_label.grid(column=0, row=2)
+username_label.grid(column=0, row=3)
 
 password_label = Label(text='Password:')
-password_label.grid(column=0, row=3)
+password_label.grid(column=0, row=4)
 
 # Entries
+search_entry = Entry(width=35)
+search_entry.grid(column=1, row=1)
+
 website_entry = Entry(width=53)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.grid(column=1, row=2, columnspan=2)
 # set cursor in website entry field
 website_entry.focus()
-website_entry.insert(0, 'https://')
+website_entry.insert(END, '')
 
 username_entry = Entry(width=53)
-username_entry.grid(column=1, row=2, columnspan=2)
+username_entry.grid(column=1, row=3, columnspan=2)
 username_entry.insert(END, 'your_fav_email@gmail.com')
 
 password_entry = Entry(width=35)
-password_entry.grid(column=1, row=3)
+password_entry.grid(column=1, row=4)
 
 # Buttons
 generate_password_button = Button(text='Generate Password', command=generate_password)
-generate_password_button.grid(column=2, row=3)
+generate_password_button.grid(column=2, row=4)
 
 add_button = Button(text='Add', width=45, command=add_password)
-add_button.grid(column=1, row=4, columnspan=2)
+add_button.grid(column=1, row=5, columnspan=2)
+
+search_button = Button(text='Search', width=13, command=search_for_website)
+search_button.grid(column=2, row=1)
 
 window.mainloop()
