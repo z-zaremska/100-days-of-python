e1736edcf14c1e49f36189b7ac67a189920315d3
z-zaremska <zuza.zaremska@gmail.com>
2023-06-10 21:02:54 +0200

    Buy max available reward after every 5 seconds.

--- Day 48 - Playing Bot Game/main.py
+++ Day 48 - Playing Bot Game/main.py
@@ -1,32 +1,60 @@
+import os
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-URL = 'https://www.python.org/'
+COOKIE_URL = 'https://orteil.dashnet.org/experiments/cookie/'
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
-chrome_driver_path = '/Users/zuzaz/Development/chromedriver.exe'
+chrome_driver_path = os.environ.get('CHROME_DRIVER')
 chrome_service = Service(executable_path=chrome_driver_path)
 driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
-driver.get(URL)
+driver.get(COOKIE_URL)
 
-events_xpath = '//*[@id="content"]/div/section/div[2]/div[2]/div/ul'
-events = driver.find_element(by=By.XPATH, value=events_xpath)
+# Game panel
+cookie_button = driver.find_element(by=By.ID, value='cookie')
 
-events = events.find_elements(by=By.TAG_NAME, value='li')
+game_start_time = time.time()
+timeout = time.time() + 60*5
+print('Start time: ', game_start_time)
 
-events_dict = {}
+while time.time() < timeout:
+    # Click the cookie.
+    cookie_button.click()
 
-for index, event in enumerate(events):
-    date, event_name = event.text.split('\n')
-    events_dict[index] = {
-        'time': date,
-        'name': event_name
-    }
+    # Every 5 seconds
+    if time.time() > game_start_time + 5:
+        # Check rewards current prices
+        rewards = driver.find_element(by=By.ID, value='store').find_elements(
+            by=By.CSS_SELECTOR, value='div > b')
 
-driver.quit()
+        # Create rewards dictionary
+        rewards_dict = {}
+        for reward in rewards[:-1]:
+            name, price = reward.text.strip().split('\n')[0].split(' - ')
+            rewards_dict[int(price.replace(',', ''))] = name
 
-print(events_dict)
+        # Check current number of cookies
+        number_of_cookies = int(driver.find_element(
+                                                    by=By.XPATH,
+                                                    value='//*[@id="money"]'
+                                                ).text.replace(',', ''))
+
+        try:
+            max_reward = [name for price, name in sorted(rewards_dict.items()) if price < number_of_cookies][-1]
+
+            buy_reward = driver.find_element(by=By.ID, value=f'buy{max_reward}')
+            buy_reward.click()
+            print(f'Bought: buy{max_reward}')
+            game_start_time += 5
+
+
+driver.close()
+
+print('Game end: ', time.time())
+cookies_per_sec = driver.find_element(by=By.ID, value='cps')
+print(f'Cookies per second: {cookies_per_sec}')
