4d7d837b0cd8eeea1d629b483da8732beb58f3ab
z-zaremska <zuza.zaremska@gmail.com>
2023-06-10 18:13:29 +0200

    Create dictionary containg events and their dates.

--- Day 48 - Playing Bot Game/main.py
+++ Day 48 - Playing Bot Game/main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
+URL = 'https://www.python.org/'
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
@@ -10,8 +11,22 @@ chrome_driver_path = '/Users/zuzaz/Development/chromedriver.exe'
 chrome_service = Service(executable_path=chrome_driver_path)
 driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
-driver.get('https://www.amazon.com')
+driver.get(URL)
 
+events_xpath = '//*[@id="content"]/div/section/div[2]/div[2]/div/ul'
+events = driver.find_element(by=By.XPATH, value=events_xpath)
 
+events = events.find_elements(by=By.TAG_NAME, value='li')
 
-# driver.quit()
+events_dict = {}
+
+for index, event in enumerate(events):
+    date, event_name = event.text.split('\n')
+    events_dict[index] = {
+        'time': date,
+        'name': event_name
+    }
+
+driver.quit()
+
+print(events_dict)
