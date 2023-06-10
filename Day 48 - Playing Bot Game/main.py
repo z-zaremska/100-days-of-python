0d9a7e625fd2a7c3c86e67ccf8d46f5e9c3768cf
z-zaremska <zuza.zaremska@gmail.com>
2023-06-10 21:24:01 +0200

    Code cleaning.

--- Day 48 - Playing Bot Game/main.py
+++ Day 48 - Playing Bot Game/main.py
@@ -17,17 +17,17 @@ driver.get(COOKIE_URL)
 
 # Game panel
 cookie_button = driver.find_element(by=By.ID, value='cookie')
+rewards_bought = 0
 
 game_start_time = time.time()
 timeout = time.time() + 60*5
-print('Start time: ', game_start_time)
+
 
 while time.time() < timeout:
     # Click the cookie.
     cookie_button.click()
 
-    # Every 5 seconds
-    if time.time() > game_start_time + 5:
+    if time.time() > game_start_time + 20:
         # Check rewards current prices
         rewards = driver.find_element(by=By.ID, value='store').find_elements(
             by=By.CSS_SELECTOR, value='div > b')
@@ -43,17 +43,18 @@ while time.time() < timeout:
                                                     by=By.XPATH,
                                                     value='//*[@id="money"]'
                                                 ).text.replace(',', ''))
+        try:
+            max_reward = [name for price, name in sorted(rewards_dict.items()) if price < number_of_cookies][-1]
 
-        max_reward = [name for price, name in sorted(rewards_dict.items()) if price < number_of_cookies][-1]
+            buy_reward = driver.find_element(by=By.ID, value=f'buy{max_reward}')
+            buy_reward.click()
+            game_start_time += 5
+            rewards_bought += 1
+        except Exception:
+            continue
 
-        buy_reward = driver.find_element(by=By.ID, value=f'buy{max_reward}')
-        buy_reward.click()
-        print(f'Bought: buy{max_reward}')
-        game_start_time += 5
-
-
+cookies_per_sec = driver.find_element(by=By.ID, value='cps')
 driver.close()
 
-print('Game end: ', time.time())
-cookies_per_sec = driver.find_element(by=By.ID, value='cps')
-print(f'Cookies per second: {cookies_per_sec}')
+print(f'Cookies per second: {cookies_per_sec.text}')
+print(f'Bought rewards: {rewards_bought}')
