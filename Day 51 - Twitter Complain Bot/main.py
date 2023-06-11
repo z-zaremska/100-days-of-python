8b08bfc5e5145703abf6690c0ec39bb40999f34e
z-zaremska <zuza.zaremska@gmail.com>
2023-06-11 18:34:32 +0200

    Created methods: log_into_account and instance initialization attributes.

--- Day 51 - Twitter Complain Bot/main.py
+++ Day 51 - Twitter Complain Bot/main.py
@@ -5,9 +5,7 @@ from twitterbot import InternetSpeedTwitterBot
 PROMISED_UP = 10
 PROMISED_DOWN = 100
 TWITTER_PASSWORD = os.environ.get('TWITTER_PASSWORD')
-TWITTER_USERNAME = os.environ.get('MY_EMAIL')
-TWITTER_URL = 'https://twitter.com'
-SPEEDTEST_URL = 'https://www.speedtest.net/pl'
+TWITTER_USERNAME = os.environ.get('TWITTER_USERNAME')
 CHROME_DRIVER = os.environ.get('CHROME_DRIVER')
 
 twitter_bot = InternetSpeedTwitterBot(
@@ -16,42 +14,18 @@ twitter_bot = InternetSpeedTwitterBot(
     driver_path=CHROME_DRIVER
 )
 
-twitter_bot.go_to_site(TWITTER_URL)
+twitter_bot.log_into_account(
+    username=TWITTER_USERNAME,
+    password=TWITTER_PASSWORD
+)
 
 
 
-
-# Go to login page
-login_page_xpath = '//*[@id="layers"]/div/div[1]/div/div/div/div/div[2]/div/div/div[1]/a'
-login_page = driver.find_element(by=By.XPATH, value=login_page_xpath)
-login_page.click()
-
-time.sleep(2)
-
-# Log into personal account
-enter_username_xpath = '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input'
-enter_username = driver.find_element(by=By.XPATH, value=enter_username_xpath)
-enter_username.send_keys(TWITTER_USERNAME)
-
-next_button_xpath = '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]/div'
-next_button = driver.find_element(by=By.XPATH, value=next_button_xpath)
-next_button.click()
-
-time.sleep(2)
-
-enter_password_xpath = '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input'
-enter_password = driver.find_element(by=By.XPATH, value=enter_password_xpath)
-enter_password.send_keys(TWITTER_PASSWORD)
-
-login_button_xpath = '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div'
-login_button = driver.find_element(by=By.XPATH, value=login_button_xpath)
-login_button.click()
-
-time.sleep(2)
-
-write_tweet_xpath = '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div/label/div[1]/div/div/div/div/div/div[2]/div/div/div/div'
-write_tweet = driver.find_element(by=By.XPATH, value=write_tweet_xpath)
-
-message = 'Elo, elo. 520...'
-
-write_tweet.send_keys(message)
+#
+#
+# write_tweet_xpath = '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div/label/div[1]/div/div/div/div/div/div[2]/div/div/div/div'
+# write_tweet = driver.find_element(by=By.XPATH, value=write_tweet_xpath)
+#
+# message = 'Elo, elo. 520...'
+#
+# write_tweet.send_keys(message)
--- Day 51 - Twitter Complain Bot/twitterbot.py
+++ Day 51 - Twitter Complain Bot/twitterbot.py
@@ -1,8 +1,13 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 
+
 class InternetSpeedTwitterBot:
+    TWITTER_URL = 'https://twitter.com'
+    SPEEDTEST_URL = 'https://www.speedtest.net/pl'
 
     def __init__(self, up: int, down: int, driver_path: str):
         self.up = up
@@ -16,12 +21,61 @@ class InternetSpeedTwitterBot:
             options=chrome_options
         )
 
+        self.go_to_site(self.TWITTER_URL)
+
     def go_to_site(self, site_url: str):
         """Opens the site from given url."""
         self.driver.get(url=site_url)
 
     def log_into_account(self, username, password):
-        pass
+        """
+        Log into personal Twitter account with given username
+        and password.
+        """
+
+        # Go to login page.
+        login_page_xpath = '//*[@id="layers"]/div/div[1]/div/div/div/div/div[2]/div/div/div[1]/a'
+        login_page = self.driver.find_element(
+            by=By.XPATH,
+            value=login_page_xpath
+        )
+        login_page.click()
+
+        time.sleep(2)
+
+        # Enter username.
+        enter_username_xpath = '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input'
+        enter_username = self.driver.find_element(
+            by=By.XPATH,
+            value=enter_username_xpath
+        )
+        enter_username.send_keys(username)
+
+        # Click on "Next" button.
+        next_button_xpath = '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]/div'
+        next_button = self.driver.find_element(
+            by=By.XPATH,
+            value=next_button_xpath
+        )
+        next_button.click()
+
+        time.sleep(2)
+
+        # Enter password.
+        enter_password_xpath = '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input'
+        enter_password = self.driver.find_element(
+            by=By.XPATH,
+            value=enter_password_xpath
+        )
+        enter_password.send_keys(password)
+
+        # Click on "Login" button.
+        login_button_xpath = '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div'
+        login_button = self.driver.find_element(
+            by=By.XPATH,
+            value=login_button_xpath
+        )
+        login_button.click()
 
     def get_internet_speed(self):
         pass
